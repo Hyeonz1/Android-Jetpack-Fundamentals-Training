30f4e186b292ecb3ec11dfeb8ddeec07a20f1b30
Hyeonz1 <hz1000509@naver.com>
2023-05-04 16:04:46 +0900

    Feat: AffirmationCard 컴포즈 함수 작성

--- Build-a-scrollable-list/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Build-a-scrollable-list/basic-android-kotlin-compose-training-affirmations-starter/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -17,8 +17,21 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,5 +46,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+    }
+}
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(8.dp), elevation = 4.dp) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+        }
     }
 }
