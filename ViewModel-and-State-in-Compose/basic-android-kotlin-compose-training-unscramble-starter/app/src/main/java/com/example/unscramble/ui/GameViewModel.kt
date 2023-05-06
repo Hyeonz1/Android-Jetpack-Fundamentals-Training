af2cdd87e7caebe438f01cffcdd7ddd91ef95cc2
Hyeonz1 <hz1000509@naver.com>
2023-05-06 22:59:31 +0900

    Feat: GameViewModel 추가

---
+++ ViewModel-and-State-in-Compose/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+}
